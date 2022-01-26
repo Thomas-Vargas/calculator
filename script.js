1d5a4eaebfc428b65f1adfd59fa75332c5d4daf5
Thomas Vargas <tavargas2012irc@gmail.com>
2022-01-26 13:40:20 -0600

    finished evaluation, delete, clear, keyboard input , period and footer.

--- script.js
+++ script.js
@@ -10,8 +10,13 @@ const operatorButtons = document.querySelectorAll("[data-operator]");
 const clearButton = document.getElementById("clear");
 const deleteButton = document.getElementById("delete");
 const equalsButton = document.getElementById("equals-btn");
+const periodButton = document.getElementById("period-btn");
 
-clearButton.addEventListener("click", resetScreen);
+clearButton.addEventListener("click", clear);
+deleteButton.addEventListener("click", deleteDigit);
+periodButton.addEventListener("click", appendPeriod);
+equalsButton.addEventListener("click", evaluate);
+window.addEventListener("keydown", keyboardInput);
 
 numberButtons.forEach((button) =>
   button.addEventListener('click', () => appendNumber(button.textContent))
@@ -28,14 +33,66 @@ function appendNumber(number) {
     currentOperationScreen.textContent += number;
 };
 
+function appendPeriod() {
+    if(currentOperationScreen.textContent.indexOf(".") === -1) {
+        currentOperationScreen.textContent = currentOperationScreen.textContent + ".";
+    } else{
+        return;
+    }
+};
+
+function deleteDigit() {
+    currentOperationScreen.textContent = currentOperationScreen.textContent
+        .toString()
+        .slice(0, -1);
+};
+
+function keyboardInput(e) {
+    if(e.key >= 0 && e.key <= 9) {
+        appendNumber(e.key)
+    } 
+    else if(e.key === "=" || e.key === "Enter") {
+        evaluate();
+    } 
+    else if(e.key === "Backspace") {
+        deleteDigit();
+    } else if(e.key === "Escape") {
+        clear();
+    } else if(e.key === "+") {
+        setOperation(e.key);
+    }else if(e.key === "-") {
+        setOperation(e.key);
+    }
+    else if(e.key === "/" || e.key === "*") {
+        setOperation(convertOperator(e.key));
+    }
+};
+
+function convertOperator(keyboardOperator) {
+    if (keyboardOperator === "/") {
+        return "÷";
+    }
+    else if(keyboardOperator === "*") {
+        return "x"
+    };
+};
+
 function resetScreen() {
     currentOperationScreen.textContent = "";
     shouldResetScreen = false;
 }; 
 
+function clear() {
+    currentOperationScreen.textContent = "0";
+    lastOperationScreen.textContent = "";
+    firstOperand = "";
+    secondOperand = "";
+    currentOperation = null;
+};
+
 function setOperation(operator) {
     if (currentOperation !== null) {
-        evalute();
+        evaluate();
     }
     firstOperand = currentOperationScreen.textContent;
     currentOperation = operator;
@@ -43,8 +100,44 @@ function setOperation(operator) {
     shouldResetScreen = true;
 };
 
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000;
+};
+
+function evaluate() {
+    if(currentOperation === null || shouldResetScreen) {
+        return;
+    } else if(currentOperation === "÷" && currentOperationScreen.textContent === "0") {
+        alert("I don't think so!");
+    };
+
+    secondOperand = currentOperationScreen.textContent;
+    currentOperationScreen.textContent = roundResult(
+        operate(currentOperation, firstOperand, secondOperand)
+    );
+    lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand}`;
+    currentOperation = null;
+};
+
 function operate(operator, num1, num2) {
-    return operator(num1, num2);
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if(operator === "-") {
+        return subtract(num1, num2);
+    } else if(operator === "x") {
+        return multiply(num1, num2);
+    } else if(operator === "÷") {
+        if (num2 === 0) {
+            return null;
+        } else {
+            return divide(num1, num2);
+        }
+    } else {
+        return null;
+    }
 };
 
 function add(num1, num2) {
